90ca9ab4127c66c9177ae4d0111d46a9ac084d38
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2013-08-23 10:35:44 +0100

    Change LD Map to cosine correlation

--- scripts/Views/GenotypeViewer/LDMap.js
+++ scripts/Views/GenotypeViewer/LDMap.js
@@ -12,7 +12,7 @@ define(["tween", "DQX/Utils"],
         var end_snp = Math.ceil(x_scale.domain()[1]);
         var genotypes = data.snp_cache.genotypes;
         if (!genotypes) return;
-        if (snp_width < 4) return;
+        if (snp_width < 2) return;
         var num_samples = data.samples.length;
         var PAB, PA, PB, A;
         console.time('LD');
@@ -23,30 +23,41 @@ define(["tween", "DQX/Utils"],
         for (var i = start_snp, end = end_snp; i < end; ++i) {
           for (var j = i+1; j < end; ++j) {
             PAB = 0; PA = 0; PB = 0;
+            var dp = 0;
             for (var s = 0; s < num_samples; ++s) {
-              if (genotypes[s].gt[i] == 0) {
+              dp += (genotypes[s].gt[i] ? 1 : -1)*(genotypes[s].gt[j] ? 1 : -1);
+              if (genotypes[s].gt[i] == 0)
                 ++PA;
-                A = true;
-              } else
-                A = false;
-              if (genotypes[s].gt[j] == 0) {
+                //A = true;
+//              } else
+              //  A = false;
+              if (genotypes[s].gt[j] == 0)
                 ++PB;
-                if (A)
-                  ++PAB;
-              }
+//                if (A)
+//                  ++PAB;
+              //}
             }
-            PAB /= num_samples; PA /= num_samples; PB /= num_samples;
-            if (PA != 1 && PA != 0 && PB != 1 && PB != 0) {
-              var DAB = PAB - (PA * PB);
-              var r2 = (DAB * DAB) / (PA*(1-PA)*PB*(1-PB));
-              ctx.fillStyle = DQX.getRGB(255, 255 - (r2 * 255), 255 - (r2 * 255));
-              ctx.fillRect(0, ld_sq_width * (j - start_snp), ld_sq_width, ld_sq_width);
-//              ctx.strokeText(Math.round(r2*100)/100, ld_sq_width/2, (ld_sq_width * (j - start_snp)) + ld_sq_width/2);
-            } else {
-              ctx.fillStyle = '#F00';//255 - (r2 * 255), 255 - (r2 * 255));
+            dp /= num_samples;
+            dp *= dp;
+            //PAB /= num_samples; PA /= num_samples; PB /= num_samples;
+            PA /= num_samples; PB /= num_samples;
+//            if (PA != 1 && PA != 0 && PB != 1 && PB != 0) {
+//              var DAB = PAB - (PA * PB);
+//              var r2 = (DAB * DAB) / (PA*(1-PA)*PB*(1-PB));
+//              ctx.fillStyle = DQX.getRGB(255, 255 - (r2 * 255), 255 - (r2 * 255));
+//              ctx.fillRect(0, ld_sq_width * (j - start_snp), ld_sq_width, ld_sq_width);
+////              ctx.strokeText(Math.round(r2*100)/100, ld_sq_width/2, (ld_sq_width * (j - start_snp)) + ld_sq_width/2);
+//            } else {
+//              ctx.fillStyle = '#F00';//255 - (r2 * 255), 255 - (r2 * 255));
+//              ctx.fillRect(0, ld_sq_width * (j - start_snp), ld_sq_width, ld_sq_width);
+//            }
+            if (!((PA == 1 || PA == 0) && (PB == 1 || PB == 0))) {
+              //dp = 1-dp;
+              ctx.fillStyle = DQX.getRGB(255, dp*255,dp*255);
               ctx.fillRect(0, ld_sq_width * (j - start_snp), ld_sq_width, ld_sq_width);
             }
-          }
+
+        }
           //Move across to the next row
           ctx.translate(ld_sq_width, 0);
         }
